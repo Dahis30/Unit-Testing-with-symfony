86f90f9bd7a019e28f610c174db146b6e7727cea
Dahis30 <mohamednadir637@gmail.com>
2024-01-27 18:25:33 +0100

    first

---
+++ tests/AppTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests ; 
+
+use PHPUnit\Framework\TestCase;
+
+class AppTest extends TestCase{
+
+
+    public function testTestsArtworking(){
+        $this->assertEquals(3,1+1+1);
+
+    }
+
+}
